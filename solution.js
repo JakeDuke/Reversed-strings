9d7440d2f8b9b654e53b1cf27afc19480135caef
JakeDuke <36011510+JakeDuke@users.noreply.github.com>
2018-04-06 16:41:19 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,10 +1,16 @@
  function solution(string) {
     let reversed_string = "";
-    let temp_array = [];
-    
-    temp_array = string.split("");
-    temp_array = temp_array.reverse();
-    reversed_string = temp_array.join("");
+if (string is even) {
+ let length = length of string;
+ for (let i = 0; length/2; i++) {
+ //swap the i'th and  (length-1)-i'th letters
+ }
+} else {
+if (string is even) {
+ let length = length of string - 1;
+ for (let i = 0; length/2; i++) {
+ //swap the i'th and  (length-1)-i'th letters
+}
     
     return reversed_string;
 }; 
