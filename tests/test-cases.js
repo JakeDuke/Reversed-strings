d693254a41932dc8dc6b5d91106657e021c2e4ef
JakeDuke <36011510+JakeDuke@users.noreply.github.com>
2018-04-06 14:37:50 +0200

    Update test-cases.js

--- tests/test-cases.js
+++ tests/test-cases.js
@@ -5,15 +5,6 @@ tests({
   },
   'car': function() {
     eq("rac", solution("car"));
-  },
-  'pencil': function() {
-    eq("licnep", solution("pencil"));
-  },
-  '8873024': function() {
-    eq("4203788", solution("8873024"));
-  },
-  '##&][&##': function() {
-    eq("##&][&##", solution("##&][&##"));
   }
 
 });
