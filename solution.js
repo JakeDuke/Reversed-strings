19436e7b0e98c68048d4e5a062378038962ec896
JakeDuke <36011510+JakeDuke@users.noreply.github.com>
2018-04-06 18:09:46 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,16 +1,20 @@
- function solution(string) {
-    let reversed_string = "";
-if (string is even) {
- let length = length of string;
- for (let i = 0; length/2; i++) {
- //swap the i'th and  (length-1)-i'th letters
- }
-} else {
-if (string is even) {
- let length = length of string - 1;
- for (let i = 0; length/2; i++) {
- //swap the i'th and  (length-1)-i'th letters
-}
-    
-    return reversed_string;
-}; 
+	function reversed_strings(str) {
+  var spl = str.split('');
+  if (spl.length % 2 === 0) {
+    let length = spl.length;
+	    for (let i = 0; i <= length/2; i++ ) {
+	      let holder = spl[i];
+	      spl[i] = spl[length - i];
+	      spl[length - i] = holder;
+	    }
+	  } else {
+	    let length = spl.length - 1;
+	    for (let i = 0; i <= length/2; i++ ) {
+	      let holder = spl[i];
+	      spl[i] = spl[length - i];
+	      spl[length - i] = holder;
+	    }
+	  }
+	  return spl.join('');
+	}
+	reversed_strings("seregaloh");
